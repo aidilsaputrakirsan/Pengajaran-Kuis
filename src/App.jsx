7e1bea38eca9c38b425c6db2198aeeaa2e50fc26
Myst-ADL <89290578+aidilsaputrakirsan@users.noreply.github.com>
2025-01-02 07:14:26 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,33 @@
 // src/App.jsx
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import Home from './pages/Home';
-import Register from './pages/Register';
 import PreTest from './pages/PreTest';
 import PostTest from './pages/PostTest';
 import Summary from './pages/Summary';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import Register from './pages/Register';
+import QuizProvider from './context/QuizContext';
 
-const App = () => {
+function App() {
   return (
-    <div className="flex flex-col min-h-screen">
-      <Navbar />
-      <main className="flex-1">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/pre-test" element={<PreTest />} />
-          <Route path="/post-test" element={<PostTest />} />
-          <Route path="/summary" element={<Summary />} />
-        </Routes>
-      </main>
-      <Footer />
-    </div>
+    <QuizProvider>
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+        <main className="flex-grow container mx-auto p-4">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/pre-test" element={<PreTest />} />
+            <Route path="/post-test" element={<PostTest />} />
+            <Route path="/summary" element={<Summary />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </QuizProvider>
   );
-};
+}
 
 export default App;
