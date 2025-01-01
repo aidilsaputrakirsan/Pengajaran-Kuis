9b52bddfa3ac782c849c309abeb0a1a0c943c43b
Myst-ADL <89290578+aidilsaputrakirsan@users.noreply.github.com>
2025-01-02 07:14:34 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,6 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Pengajaran-Kuis/', // Sesuaikan dengan nama repository Anda
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
 });
